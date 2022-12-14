1466cc6456e90b23f4fbab3e21a346f5ab2833e2
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2022-12-14 18:11:53 +0000

    Revert "kjk"
    This reverts commit 5c202d80544fa7552dc6d94c0bc9968bd0ae0f8f.

--- src/TransmitData/SendData.java
+++ src/TransmitData/SendData.java
@@ -229,9 +229,9 @@ public class SendData {
             }
             else {
 
-            byte[] restParents = new byte[data.length-8];
+            byte[] restParents = new byte[data.length-12];
 
-            System.arraycopy(data, 8, restParents, 0, restParents.length);
+            System.arraycopy(data, 12, restParents, 0, restParents.length);
 
             byte[] bytes = ByteBuffer.allocate(restParents.length+4*2+100).
                     putInt(Constants.wakeUpClient).
